6276685b7e44cf79419bccf52f4de2c647d30cd7
IvanBoichuk <boy4uk.i@gmail.com>
2022-10-14 18:38:52 +0300

    Add Form

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee registration in the FOP Boichuk company </h1>
+            <h2>Total employees: </h2>
+            <h2>Receive a premium: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
