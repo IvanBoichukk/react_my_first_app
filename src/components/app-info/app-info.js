98ebb3101626c8b1df46f7c751ccee0f3afd9ca2
IvanBoichuk <boy4uk.i@gmail.com>
2022-10-19 16:10:13 +0300

    Employees form

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,12 +1,12 @@
 
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({employees, increased}) => {
     return (
         <div className="app-info">
             <h1>Employee registration in the FOP Boichuk company </h1>
-            <h2>Total employees: </h2>
-            <h2>Receive a premium: </h2>
+            <h2>Total employees: {employees}</h2>
+            <h2>Receive a premium: {increased}</h2>
         </div>
     )
 }
